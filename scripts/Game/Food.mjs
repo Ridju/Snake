7b54e3f850a9f3b90d5764074124347464519ef1
Ridju <tmp@tmp.com>
2022-02-15 09:17:13 +0100

    added colors and descriptions

--- scripts/Game/Food.mjs
+++ scripts/Game/Food.mjs
@@ -1,23 +1,26 @@
 class Food {
-    constructor(color, size, canvas) { 
-        this.color = color;
-        this.size = size;
-        this.canvas = canvas;
-        this.x = 16;
-        this.y = 16;
-    }
+  constructor(color, size, canvas) {
+    this.color = color;
+    this.size = size;
+    this.canvas = canvas;
+    this.x = 16;
+    this.y = 16;
+  }
 
-    draw(ctx) { 
-        ctx.fillRect(this.x, this.y, this.size, this.size);
-        ctx.fillStyle = this.color;
-        ctx.fill()
-    }
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+    ctx.fill();
+  }
 
-    genNew(){ 
-        this.x = Math.floor(Math.random() * Math.floor(this.canvas.width / this.size)) * this.size;
-        this.y = Math.floor(Math.random() * Math.floor(this.canvas.height / this.size)) * this.size;
-    }
-
+  genNew() {
+    this.x =
+      Math.floor(Math.random() * Math.floor(this.canvas.width / this.size)) *
+      this.size;
+    this.y =
+      Math.floor(Math.random() * Math.floor(this.canvas.height / this.size)) *
+      this.size;
+  }
 }
 
 export default Food;
--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,10 +1,10 @@
-import {Snake, Food, Game} from './Game/index.mjs';
+import { Snake, Food, Game } from "./Game/index.mjs";
 
-const GRIDDIMENSIONS = {x: 20, y: 20}
+const GRIDDIMENSIONS = { x: 20, y: 20 };
 const PIXELSIZE = 16;
 const BACKGROUNDCOLOR = "black";
 const SNAKECOLOR = "white";
-const FOODCOLOR = "orange";
+const FOODCOLOR = "red";
 
 const canvas = document.getElementById("icanvas");
 const reloadBtn = document.getElementById("reloadBtn");
@@ -20,19 +20,19 @@ var food = new Food(FOODCOLOR, PIXELSIZE, canvas);
 var snake = new Snake(SNAKECOLOR, PIXELSIZE);
 var game = new Game(canvas, snake, food, BACKGROUNDCOLOR);
 
-function reloadGame() { 
-    food = new Food(FOODCOLOR, PIXELSIZE, canvas);
-    snake = new Snake(SNAKECOLOR, PIXELSIZE);
-    game = new Game(canvas, snake, food, BACKGROUNDCOLOR);
+function reloadGame() {
+  food = new Food(FOODCOLOR, PIXELSIZE, canvas);
+  snake = new Snake(SNAKECOLOR, PIXELSIZE);
+  game = new Game(canvas, snake, food, BACKGROUNDCOLOR);
 }
 
 function main() {
-    gameOverDisplay.hidden = !game.gameOver;
-    if(!game.gameOver) { 
-        game.loop();
-        points.innerHTML = game.points;
-    }
-    setTimeout(requestAnimationFrame, 100, main);
+  gameOverDisplay.hidden = !game.gameOver;
+  if (!game.gameOver) {
+    game.loop();
+    points.innerHTML = game.points;
+  }
+  setTimeout(requestAnimationFrame, 100, main);
 }
 
 main();
